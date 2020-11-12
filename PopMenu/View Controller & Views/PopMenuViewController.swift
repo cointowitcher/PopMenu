5adab0474ec970a97596956c69de91180553f3ea
Sergey Reznichenko <mutexlock64@gmail.com>
2020-11-12 18:17:52 +0200

    popup background is not transparent

--- PopMenu/View Controller & Views/PopMenuViewController.swift
+++ PopMenu/View Controller & Views/PopMenuViewController.swift
@@ -286,7 +286,7 @@ extension PopMenuViewController {
         if colors.count > 0 {
             if colors.count == 1 {
                 // Configure solid fill background.
-                contentView.backgroundColor = colors.first?.withAlphaComponent(0.9)
+                contentView.backgroundColor = colors.first
                 contentView.startColor = .clear
                 contentView.endColor = .clear
             } else {
